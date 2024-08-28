040cfa2ebdd626fc19b4bb8341386c05ce17ee44
rppp <88567952@qq.com>
2024-08-28 14:54:35 +0800

    fix

--- scan_dir.py
+++ scan_dir.py
@@ -1,4 +1,14 @@
+import os
 
+# 获取当前目录
+current_dir = os.getcwd()
+
+# 查找所有的 .htm 和 .html 文件
+html_files = [f for f in os.listdir(current_dir) if f.endswith(('.htm', '.html'))]
+print(html_files)
+
+# 创建 index.html 文件内容
+index_content = """
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
@@ -34,19 +44,21 @@
 </head>
 <body>
     <ul>
-        <li><a href="audio.html">audio.html</a></li>
-        <li><a href="gen_pic.htm">gen_pic.htm</a></li>
-        <li><a href="client2.html">client2.html</a></li>
-        <li><a href="webphoto.html">webphoto.html</a></li>
-        <li><a href="text_to_audio.htm">text_to_audio.htm</a></li>
-        <li><a href="test.html">test.html</a></li>
-        <li><a href="speech_to_text.htm">speech_to_text.htm</a></li>
-        <li><a href="search.htm">search.htm</a></li>
-        <li><a href="a.htm">a.htm</a></li>
-        <li><a href="js.htm">js.htm</a></li>
-        <li><a href="cube.htm">cube.htm</a></li>
-        <li><a href="index2.html">index2.html</a></li>
+"""
 
+# 为每个文件添加链接
+for file in html_files:
+    if file != 'index.html':  # 排除index.html自身
+        index_content += f'        <li><a href="{file}">{file}</a></li>\n'
+
+index_content += """
     </ul>
 </body>
 </html>
+"""
+
+# 写入 index.html 文件
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(index_content)
+
+print("index.html 已成功生成。")
